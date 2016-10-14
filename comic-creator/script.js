32620ff8c425f5eceadac82cba63f68ecd45d9bf
Tyler Small <tylersmall.user@gmail.com>
2016-10-14 03:47:17 -0500

    Finish design for UI

--- comic-creator/script.js
+++ comic-creator/script.js
@@ -1,11 +1,11 @@
-var $comicCanvas = $('#comic-canvas');
+// var $comicCanvas = $('#comic-canvas');
 
-$comicCanvas.drawRect({
-  fillStyle: 'steelblue',
-  strokeStyle: 'blue',
-  strokeWidth: 1,
-  x: 0, y: 0,
-  fromCenter: false,
-  width: 100,
-  height: 50
-});
+// $comicCanvas.drawRect({
+//   fillStyle: 'steelblue',
+//   strokeStyle: 'blue',
+//   strokeWidth: 1,
+//   x: 0, y: 0,
+//   fromCenter: false,
+//   width: 100,
+//   height: 50
+// });
