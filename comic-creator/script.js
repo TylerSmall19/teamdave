2de8a134b920d40b8ed4afab5b943b69e0fcf6ac
Tyler Small <tylersmall.user@gmail.com>
2016-10-26 14:31:05 -0500

    Rename third BG image. Add events to first and last image

--- comic-creator/script.js
+++ comic-creator/script.js
@@ -1,45 +1,64 @@
 var $comicCanvas = $('#comic-canvas'); // <canvas id="comic-canvas" height="590" width="767">
-var $sideCanvas = $('#sidebar-canvas'); // <canvas id="sidebar-canvas" width="180" height="693">
+var $sideCanvas = $('#sidebar-canvas'); // <canvas id="sidebar-canvas" width="180" height="742">
 var $savedComicCanvas = $('#saved-comic-canvas'); //<canvas id="saved-comic-canvas" width="767" height="129">
 var hasBackground = false;
 
-// Wrap image in an event handler
-$(	//Draw side-bar image
-	$sideCanvas.drawImage({
+//Function variables for layer events
+var drawFunction = function draw(layer){
+	console.log(layer)
+}
+
+function addImage(canvas, src, x, y, h, w, fnc, name){
+	canvas.addLayer({
+		source: src,
+		name: name,
+		x: x, y: y,
+		height: h, width: w,
+		click: fnc
+	});
+}
+
+// var src = findSrc(canvas);
+
+
+$sideCanvas.drawImage({
+	name: "bg1",
+	layer: true,
 	source: "img/bg-1.png",
 	x: 3, y: 3,
-	width: 50, 
-	height: 50,
-	fromCenter: false
-	//Add double click event to image
-	})
-//Only allows one event to populate per element
-).click(function(){ 
-    $comicCanvas.drawImage({
-	source: "img/bg-1.png",
+	width: 50, height: 50,
 	fromCenter: false,
-	draggable: true
-	})
-	hasBackground = true;
+	click: drawFunction
 });
 
-// Wrap image in an event handler
-$(	//Draw side-bar image
-	$sideCanvas.drawImage({
+$sideCanvas.drawImage({
 	source: "img/bg-2.png",
-	x: 56, y: 3,
-	width: 50, 
-	height: 50,
+	layer: true,
+	name: "bg2",
+	x: 61, y: 3,
+	width: 50, height: 50,
 	fromCenter: false
 	//Add double click event to image
-	})
-//Only allows one event to populate per element
-).click(function(){ 
-	if (!hasBackground){
-	    $comicCanvas.drawImage({
-		source: "img/bg-2.png",
-		fromCenter: false,
-		draggable: true
-		})
-	}
 });
+
+$sideCanvas.addLayer({
+	name: 'bg3',
+	type: 'image',
+	source: 'img/bg-3.jpg',
+	x: 119, y: 3,
+	width: 50, height: 50,
+	fromCenter: false,
+	click: drawFunction
+})
+.drawLayers();
+
+
+// ).click(function(){ 
+// 	if (!hasBackground){
+// 	    $comicCanvas.drawImage({
+// 		source: "img/bg-2.png",
+// 		fromCenter: false,
+// 		draggable: true
+// 		})
+// 	}
+// });
