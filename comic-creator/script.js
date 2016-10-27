7988cb5da98fa9f1b735cfe02b4cf3d9e79bc444
Tyler Small <tylersmall.user@gmail.com>
2016-10-27 01:03:46 -0500

    Add events to sidebar thumbnails to draw background

--- comic-creator/script.js
+++ comic-creator/script.js
@@ -1,19 +1,11 @@
-
-
+$.jCanvas.defaults.fromCenter = false;
 var $comicCanvas = $('#comic-canvas'); // <canvas id="comic-canvas" height="590" width="767">
 var $sideCanvas = $('#sidebar-canvas'); // <canvas id="sidebar-canvas" width="180" height="742">
 var $savedComicCanvas = $('#saved-comic-canvas'); //<canvas id="saved-comic-canvas" width="767" height="129">
 var sideCanvasArray = [];
 var hasBackground = false;
-var src = ['img/bg-1.png', 'img/bg-2.png'];
+var src = ['img/bg1.png', 'img/bg2.png', 'img/bg3.jpg'];
 
-//Function variables for layer events
-var drawFunction = function draw(){
-	$comicCanvas.drawImage({
-		source: "img/bg-1.png",
-		width: 767, height: 575
-	})
-}
 
 function findPos(imgNum, size, padding){
 	var pos;
@@ -21,54 +13,24 @@ function findPos(imgNum, size, padding){
 	return pos;
 }
 
-for (i = 0; i < src.length; i++){
+for (x = 0; x < src.length; x++){
+	var y = x;
+	var name = 'bg'+x;
 	$sideCanvas.addLayer({
 		type: 'image',
-		name: src[i],
-		source: src[i],
-		x: findPos(i, 50, 3), y: 3,
-		height: 50, width: 50
+		name: name,
+		source: src[x],
+		x: findPos(x, 55, 3), y: 3,
+		height: 55, width: 55,
+		click: function(layer){
+			$comicCanvas.addLayer({
+				type:'image',
+				source: layer.source,
+				x: 0, y: 0,
+				draggable: false
+			}).drawLayers();
+		}
 	})
-	$sideCanvas.addLayerToGroup(src[i], 'backgrounds')
+	$sideCanvas.addLayerToGroup(name, 'backgrounds')
 }
 $sideCanvas.drawLayers();
-console.log($sideCanvas.getLayerGroup('backgrounds'))
-
-// $sideCanvas.drawImage({
-// 	name: "bg1",
-// 	layer: true,
-// 	source: "img/bg-1.png",
-// 	x: 3, y: 3,
-// 	width: 50, height: 50,
-// 	fromCenter: false,
-// 	click: function draw(){
-// 		$comicCanvas.drawImage({
-// 			source: src,
-// 			width: 767, height: 575
-// 		})
-// 	}
-// });
-
-// $sideCanvas.drawImage({
-// 	source: "img/bg-2.png",
-// 	layer: true,
-// 	name: "bg2",
-// 	x: 61, y: 3,
-// 	width: 50, height: 50,
-// 	fromCenter: false
-// 	//Add double click event to image
-// });
-
-// $sideCanvas.addLayer({
-// 	name: 'bg3',
-// 	type: 'image',
-// 	source: 'img/bg-3.jpg',
-// 	x: 119, y: 3,
-// 	width: 50, height: 50,
-// 	fromCenter: false,
-// 	click: drawFunction
-// })
-// .drawLayers();
-
-
-
