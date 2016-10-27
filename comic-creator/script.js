158ad7ed2d1239fa7a9e423a075b1ec0dea341bd
Tyler Small <intrasolarmusic@gmail.com>
2016-10-26 19:33:51 -0500

    Begin logic for dynamic layer creation

--- comic-creator/script.js
+++ comic-creator/script.js
@@ -1,64 +1,74 @@
+
+
 var $comicCanvas = $('#comic-canvas'); // <canvas id="comic-canvas" height="590" width="767">
 var $sideCanvas = $('#sidebar-canvas'); // <canvas id="sidebar-canvas" width="180" height="742">
 var $savedComicCanvas = $('#saved-comic-canvas'); //<canvas id="saved-comic-canvas" width="767" height="129">
+var sideCanvasArray = [];
 var hasBackground = false;
+var src = ['img/bg-1.png', 'img/bg-2.png'];
 
 //Function variables for layer events
-var drawFunction = function draw(layer){
-	console.log(layer)
+var drawFunction = function draw(){
+	$comicCanvas.drawImage({
+		source: "img/bg-1.png",
+		width: 767, height: 575
+	})
 }
 
-function addImage(canvas, src, x, y, h, w, fnc, name){
-	canvas.addLayer({
-		source: src,
-		name: name,
-		x: x, y: y,
-		height: h, width: w,
-		click: fnc
-	});
+function findPos(imgNum, size, padding){
+	var pos;
+	pos = imgNum * size + padding;
+	return pos;
 }
 
-// var src = findSrc(canvas);
+for (i = 0; i < src.length; i++){
+	$sideCanvas.addLayer({
+		type: 'image',
+		name: src[i],
+		source: src[i],
+		x: findPos(i, 50, 3), y: 3,
+		height: 50, width: 50
+	})
+	$sideCanvas.addLayerToGroup(src[i], 'backgrounds')
+}
+$sideCanvas.drawLayers();
+console.log($sideCanvas.getLayerGroup('backgrounds'))
 
-
-$sideCanvas.drawImage({
-	name: "bg1",
-	layer: true,
-	source: "img/bg-1.png",
-	x: 3, y: 3,
-	width: 50, height: 50,
-	fromCenter: false,
-	click: drawFunction
-});
-
-$sideCanvas.drawImage({
-	source: "img/bg-2.png",
-	layer: true,
-	name: "bg2",
-	x: 61, y: 3,
-	width: 50, height: 50,
-	fromCenter: false
-	//Add double click event to image
-});
-
-$sideCanvas.addLayer({
-	name: 'bg3',
-	type: 'image',
-	source: 'img/bg-3.jpg',
-	x: 119, y: 3,
-	width: 50, height: 50,
-	fromCenter: false,
-	click: drawFunction
-})
-.drawLayers();
-
-
-// ).click(function(){ 
-// 	if (!hasBackground){
-// 	    $comicCanvas.drawImage({
-// 		source: "img/bg-2.png",
-// 		fromCenter: false,
-// 		draggable: true
+// $sideCanvas.drawImage({
+// 	name: "bg1",
+// 	layer: true,
+// 	source: "img/bg-1.png",
+// 	x: 3, y: 3,
+// 	width: 50, height: 50,
+// 	fromCenter: false,
+// 	click: function draw(){
+// 		$comicCanvas.drawImage({
+// 			source: src,
+// 			width: 767, height: 575
 // 		})
 // 	}
 // });
+
+// $sideCanvas.drawImage({
+// 	source: "img/bg-2.png",
+// 	layer: true,
+// 	name: "bg2",
+// 	x: 61, y: 3,
+// 	width: 50, height: 50,
+// 	fromCenter: false
+// 	//Add double click event to image
+// });
+
+// $sideCanvas.addLayer({
+// 	name: 'bg3',
+// 	type: 'image',
+// 	source: 'img/bg-3.jpg',
+// 	x: 119, y: 3,
+// 	width: 50, height: 50,
+// 	fromCenter: false,
+// 	click: drawFunction
+// })
+// .drawLayers();
+
+
+
