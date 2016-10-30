65f0852e1a4a3e43dbf3468c1b1496195c210218
ollieshmollie <oliverduncan@icloud.com>
2016-10-29 19:47:43 -0500

    Add click handlers to ui-buttons

--- comic-creator/canvas.js
+++ comic-creator/canvas.js
@@ -1,11 +1,7 @@
 $.jCanvas.defaults.fromCenter = false; // Sets default draw start point to be from upper left corner
 var $comicCanvas = $('#comic-canvas'); // <canvas id="comic-canvas" height="590" width="767">
 
-$(document).ready(function() {
-  layoutUIButtons();
-});
-
-// -------- CANVAS SETUP --------
+// -------- UI BUTTON SETUP --------
 
 function layoutUIButtons(src, x, y) {
   var imgNames = [
@@ -44,7 +40,7 @@ function layoutUIButtons(src, x, y) {
   $comicCanvas.drawLayers();
 };
 
-// -------- HELPER FUNCTIONS --------
+// -------- CLICK HANDLERS --------
 
 function bringSelectedLayersToFront() {
   // TODO
@@ -62,3 +58,30 @@ function deleteSelectedLayers() {
   // Redraws layers (required on layer remove, option most other places. Test necessity before altering)
   .drawLayers();
 }
+
+// -------- RUN SCRIPT --------
+
+$(document).ready(function() {
+  
+  layoutUIButtons();
+  
+  // Add click handlers
+  $comicCanvas.setLayer('delete-button', {
+    click: function(layer) {
+      deleteSelectedLayers();
+    }
+  });
+
+  $comicCanvas.setLayer('text-button', {
+    click: function(layer) {
+      addTextLayer();
+    }
+  });
+
+  $comicCanvas.setLayer('bring-to-front', {
+    click: function(layer) {
+      bringSelectedLayersToFront();
+    }
+  })
+
+});
