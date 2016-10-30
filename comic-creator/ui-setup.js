248cdaacdb20aa01d838e36239fe727d559d4b06
ollieshmollie <oliverduncan@icloud.com>
2016-10-30 14:46:33 -0500

    Remove redundant variable declaration

--- comic-creator/ui-setup.js
+++ comic-creator/ui-setup.js
@@ -1,5 +1,3 @@
-$.jCanvas.defaults.fromCenter = false; // Sets default draw start point to be from upper left corner
-var $comicCanvas = $('#comic-canvas'); // <canvas id="comic-canvas" height="590" width="767">
 
 layoutUIButtons();
   
