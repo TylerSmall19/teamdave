3cfbbb9bce6a164da7dcd5306be8b13cf6a6c78d
ollieshmollie <oliverduncan@icloud.com>
2016-11-04 23:23:29 -0500

    Reorganize files

--- comic-creator/server.js
+++ comic-creator/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var server = express();
 
 server.get('/', function(request, response) {
-  response.sendFile('./index.html', {root: __dirname}, function(error) {
+  response.sendFile('public/index.html', {root: __dirname}, function(error) {
     if (error) {
       console.log(error);
       response.status(error.status).end();
@@ -10,7 +10,8 @@ server.get('/', function(request, response) {
   });
 });
 
-server.use(express.static('.'));
+server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public/img'))
 
 server.listen(3000, function() {
   console.log('Server is listening on port 3000');
